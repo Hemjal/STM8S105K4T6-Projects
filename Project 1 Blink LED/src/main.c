0f0300f2021c951ccc53009e7c3e59d0bc0c2d04
Hemjal <abuhemjalharun@yahoo.com>
2021-04-01 00:12:17 +0300

    Blinking On board LED

---
+++ Project 1 Blink LED/src/main.c
@@ -0,0 +1,57 @@
+/**
+******************************************************************************
+Project: Blink On Board LED
+Author: Md Abu Hemjal
+Date: 31st March 2021
+******************************************************************************
+*/
+
+#include "stm8s.h"
+
+
+#define LED_DELAY_MS 100
+
+void CLK_Configuration(void);
+void GPIO_Configuration(void);
+void Toggle(void);
+
+
+void main(void)
+{
+  /* Configures clocks */
+  CLK_Configuration();
+  
+  /* Configures GPIOs */
+  GPIO_Configuration();
+  
+  while(1)
+	{
+	GPIO_WriteHigh(GPIOE, GPIO_PIN_5);
+	delay_ms(LED_DELAY_MS);
+	GPIO_WriteLow(GPIOE, GPIO_PIN_5);
+	delay_ms(LED_DELAY_MS);	
+	}
+  
+}
+
+
+void CLK_Configuration(void)
+{
+  
+  /* Fmaster = 16MHz */
+  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+  
+}
+
+void GPIO_Configuration(void)
+{
+  GPIO_DeInit(GPIOE);
+  GPIO_Init(GPIOE, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST);
+}
+
+
+void Toggle(void)
+{
+  GPIO_WriteReverse(GPIOD, GPIO_PIN_0);
+}
+/****************** (c) 2008  STMicroelectronics ******************************/
